89fc334b2d3269ed143cf557df7da36062ba8b87
akaValmi <116489251+akaValmi@users.noreply.github.com>
2023-05-06 00:42:14 -0600

    Delete src directory

--- src/routes/productosRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { agregarProductos, obtenerProductos } from "../controllers/productoController";
-
-const router = express.Router();
-
-router.post("/registroProductos", agregarProductos);
-router.get("/", obtenerProductos);
-
-export default router;
